a6f8cb83837f7dc5f464c55896647ed8b1596f56
Sourav Nag <sn.sourav1993@gmail.com>
2024-03-31 13:14:54 +0530

    03 Episode 3 - Laying the foundation

--- App.js
+++ App.js
@@ -1,26 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "heading" }, "Hello World from React"), // make it array to pass siblings
-    React.createElement("h1", { id: "heading2" }, "This is a h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "Hello World from React - child 2"), // make it array to pass siblings
-    React.createElement("h1", {}, "This is a h2 tag - child 2"),
-  ]),
-]);
+// React.createElement => ReactElement-JS Object => HTML Element (when we render into dom)
 
-// creating React Element
-// What is React Element? => It is a javaScript Object!
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello World from React!"
-// );
+// const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
 
-console.log(parent);
+// console.log(heading);
+
+// JSX - HTML like syntax or more like XML but not HTML or XML. JSX is a react element. which creates a object.
+// JSX transpiled before it reaches the JS Engine. What is transpiled? Tranpiled means this code will converted to the code which browser can understand
+// Transpiling is done by PARCEL. PARCEL has bable. and bable job is to transpiled JSX into ReactElement.
+
+// JSX => React.createElement => ReactElement-JS Object => HTML Element (when we render into dom)
+// React Element
+
+const Title = () => (
+  <h1 id="heading" className="head">
+    Namaste React using JSX
+  </h1>
+);
+
+// console.log(heading);
+
+// Reacct Component
+// Class based component - OLD
+// Functional Component - NEW
+
+// React Functional Component - Just a normal javascript function which return a React Element. Afunction which is returing some JSX is functional component.
+
+const number = 1000;
+const HeadingComponent = () => {
+  return (
+    <div id="ccontainer">
+      <Title />
+      {Title()}
+      {console.log(number, "Test2")}
+      <h1>Namaste React Functional Component</h1>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent); // this render method will convert the 'heading' object into a h1 tag and put inside dom
+// root.render(heading);
+root.render(<HeadingComponent />);
